02a40b8f65c68c04058bc46f43920abf990cc095
Guilherme Baufaker Rêgo <gbaufake@redhat.com>
2021-03-28 19:55:48 -0300

    Adding the Repeat for Battle School

--- src/test/gatling/BattleSchool.scala
+++ src/test/gatling/BattleSchool.scala
@@ -13,7 +13,6 @@ import scala.util.parsing.json.JSON
 class BattleSchool extends Simulation {
 
   val host: String = sys.env("SOCKET_ADDRESS")
-  val listA = (0 to 5) zip (0 to 5) zip  (0 to 1)
   val numUsers: Int = sys.env("USERS").toInt
   val numAttacks: Int = sys.env("ATTACKS").toInt
   val wsProtocol: String = sys.env("WS_PROTOCOL")
@@ -76,28 +75,29 @@ class BattleSchool extends Simulation {
 
   object Attack {
 
-
     val checkAttack = ws.checkTextMessage("CheckAttack")
       .matching(
-        jsonPath("$.type").is("attack-result")
+        jsonPath("$.data.match.activePlayer").is("${playerId}")
       )
       .check(jsonPath("$").saveAs("attack-result"))
 
 
     val attack: ChainBuilder =
-      repeat(numAttacks, "i") {
+      repeat(numAttacks, "index") {
         exec(session => {
-          val r = scala.util.Random
-          val x_axis = r.nextInt(4) + 1
-          val y_axis = r.nextInt(4) + 1
+          val index = session("index").as[Int]
+          val numPairs = List((0, 0), (0, 1), (0, 2), (0,3), (0,4), (1, 0), (1, 1), (1, 2), (1,3), (1,4), (2, 0), (2, 1), (2, 2), (2,3), (2,4), (3, 0), (3, 1), (3, 2), (3,3), (3,4), (4, 0), (4, 1), (4, 2), (4,3), (4,4))
+          val x_axis = numPairs(index)._1
+          val y_axis = numPairs(index)._2
           session
             .set("x_axis", x_axis)
             .set("y_axis", y_axis)
         })
           .exec(ws("Attack")
-            .sendText("""{"type":"attack","data":{"type":"1x1","origin":["${x_axis}","${y_axis}"],"orientation":"horizontal"}}""")
+            .sendText("""{"type":"attack","data":{"type":"1x1","origin":["${x_axis}","${y_axis}"],"orientation":"horizontal"}}""").await(5.seconds)(checkAttack)
           )
           .pause(5)
+          print(session("attack-result"))
       }
   }
 }
